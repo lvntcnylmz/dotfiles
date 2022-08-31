0a29d5678169d1071e20f7532366f1571082e7e7
Levent Can Yilmaz <leventcanyilmaz@proton.me>
2022-08-31 11:22:40 +0200

    added fish shell config files

---
+++ .config/fish/config.fish
@@ -0,0 +1,5 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    neofetch
+    oh-my-posh init fish --config /home/can/Documents/github/oh-my-posh/ohmyposh.omg.json | source
+end
