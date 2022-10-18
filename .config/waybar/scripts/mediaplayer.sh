46219c38bcb702be9cfb4a16b0e75b69722025b1
Levent Can Yilmaz <leventcanyilmaz@proton.me>
2022-10-18 16:35:35 +0200

    added waybar script

---
+++ .config/waybar/scripts/mediaplayer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+player_status=$(playerctl -p %any status)
+player_name=$(playerctl metadata --format "{{ playerName }}")
+song=$(playerctl metadata --format "{{ artist }}\n{{ title }}\n{{ album }}")
+
+
+if [ "$player_status" = "Playing" ] || [ "$player_status" = "Paused" ]
+then
+    if [ "$player_name" = "spotify" ]
+    then
+        echo "$(playerctl metadata --format "{{ emoji(status) }} {{ artist }} - {{ title }} - {{ duration(position) }} / {{ duration(mpris:length) }}")"
+    else
+        echo "$(playerctl metadata --format "{{ emoji(status) }} {{ artist }} - {{ title }}")"
+    fi
+fi
