8c0ae468e55a5e31b21475bc92087c4adbde7439
Levent Can Yilmaz <leventcanyilmaz@proton.me>
2022-10-18 21:42:20 +0200

    deleted old scripts

--- .config/waybar/dunst.sh
+++
@@ -1,5 +0,0 @@
-COUNT=$(dunstctl count waiting)
-ENABLED=
-DISABLED=
-if [ $COUNT != 0 ]; then DISABLED=" $COUNT"; fi
-if dunstctl is-paused | grep -q "false" ; then echo $ENABLED; else echo $DISABLED; fi
