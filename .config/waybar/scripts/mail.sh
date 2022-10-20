38649c84b1678a50412f72a2404e137dfa7f931c
Levent Can Yilmaz <leventcanyilmaz@proton.me>
2022-10-20 18:58:06 +0200

    updated mail script

--- .config/waybar/scripts/mail.sh
+++ .config/waybar/scripts/mail.sh
@@ -2,7 +2,7 @@
 
 # Server and mail adress are defined with credentials in ~/.netrc
 SERVER="$(head -1 ~/.netrc | awk '{print $2}')"
-INBOX=$(curl -fk --netrc -X "STATUS INBOX (UNSEEN)" imaps://$SERVER/INBOX | tr -d -c "[:digit:]")
+INBOX=$(curl --netrc -X "STATUS INBOX (UNSEEN)" imaps://$SERVER/INBOX | tr -d -c "[:digit:]")
 
 if [ $INBOX ] && [ $INBOX -gt 0 ] ; then
     if [ $INBOX -eq 1 ] ; then
