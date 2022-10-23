c082317995e4d4cde56f4dd127027486f769cb4f
Levent Can Yilmaz <leventcanyilmaz@proton.me>
2022-10-23 13:24:07 +0200

    updated update script

--- .config/waybar/scripts/checkupdates.sh
+++ .config/waybar/scripts/checkupdates.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-updates=$(yay -Qu | wc -l)
+updates_yay=$(yay -Qu --aur 2> /dev/null | wc -l)
+updates_pacman=$(checkupdates 2> /dev/null | wc -l)
+
+updates=$((updates_pacman + updates_yay))
 
 if [ "$updates" -gt 0 ]
 then 
     echo "$updates"
-    notify-send -u critical -i software-update-available-symbolic "Arch Linux" "$updates_yay Update Available"
+    notify-send -u normal -i software-update-available-symbolic "Arch Linux" "$updates Update/s Available"
 else
     echo "0"
 fi
