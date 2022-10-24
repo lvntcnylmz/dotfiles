886e57aa6636e7c7afab9e8907a5651059660f6f
Levent Can Yilmaz <leventcanyilmaz@proton.me>
2022-10-24 14:33:06 +0200

    updated script

--- .config/waybar/scripts/checkupdates.sh
+++ .config/waybar/scripts/checkupdates.sh
@@ -5,10 +5,23 @@ updates_pacman=$(checkupdates 2> /dev/null | wc -l)
 
 updates=$((updates_pacman + updates_yay))
 
-if [ "$updates" -gt 0 ]
-then 
-    echo "$updates"
-    notify-send -u normal -i software-update-available-symbolic "Arch Linux" "$updates Update/s Available"
+if [ "$updates" -gt 0 ]; then
+    if [ "$updates_yay" -eq 1 ]; then
+        echo "1"
+        notify-send -u normal -i software-update-available-symbolic "Arch Linux" "$updates_yay update available from AUR"
+    fi 
+    if [ "$updates_pacman" -eq 1 ]; then
+        echo "1"
+        notify-send -u normal -i software-update-available-symbolic "Arch Linux" "$updates_pacman update available from pacman"
+    fi
+    if [ "$updates_yay" -gt 1 ]; then
+        echo "$updates_yay"
+        notify-send -u normal -i software-update-available-symbolic "Arch Linux" "$updates_yay updates available from AUR"
+    fi 
+    if [ "$updates_pacman" -gt 1 ]; then
+        echo "$updates_pacman"
+        notify-send -u normal -i software-update-available-symbolic "Arch Linux" "$updates_pacman updates available from AUR"
+    fi 
 else
     echo "0"
 fi
