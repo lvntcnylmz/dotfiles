a0c4da714803647f31ef0006bfeefbf5615cd841
Levent Can Yilmaz <leventcanyilmaz@proton.me>
2022-11-05 22:21:13 +0100

    updated checkupdates script

--- .config/waybar/scripts/checkupdates.sh
+++ .config/waybar/scripts/checkupdates.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
 updates_yay=$(yay -Qu --aur 2> /dev/null | wc -l)
-updates_pacman=$(pacman -Qu 2> /dev/null | wc -l)
-
+updates_pacman=$(checkupdates 2> /dev/null | wc -l)
 updates=$((updates_pacman + updates_yay))
 
-if [ "$updates" -ge 1 ]; then
+if [ "$updates" -gt 0 ]; then
     if [ "$updates_yay" -eq 1 ]; then
-        echo "1"
+        echo "<span foreground='#ff6600'><big></big> $updates</span>"
         notify-send -u normal -i software-update-available-symbolic "Arch Linux" "$updates_yay update available from AUR"
     fi 
     if [ "$updates_pacman" -eq 1 ]; then
-        echo "1"
+        echo "<span foreground='#ff6600'><big></big> $updates</span>"
         notify-send -u normal -i software-update-available-symbolic "Arch Linux" "$updates_pacman update available from pacman"
     fi
     if [ "$updates_yay" -gt 1 ]; then
-        echo "$updates"
+        echo "<span foreground='#ff6600'><big></big> $updates</span>"
         notify-send -u normal -i software-update-available-symbolic "Arch Linux" "$updates_yay updates available from AUR"
     fi 
     if [ "$updates_pacman" -gt 1 ]; then
-        echo "$updates"
+        echo "<span foreground='#ff6600'><big></big> $updates</span>"
         notify-send -u normal -i software-update-available-symbolic "Arch Linux" "$updates_pacman updates available from pacman"
     fi 
 else
-    echo "0"
+    echo "<big></big> 0"
 fi
