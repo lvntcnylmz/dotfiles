bec68f53af48bdbf23b1121084c51120754a1fb9
Levent Can Yilmaz <leventcanyilmaz@proton.me>
2022-12-24 12:09:43 +0100

    added weather script

---
+++ .config/waybar/scripts/weather.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+for i in {1..5}
+do
+    text=$(curl -s "https://wttr.in/$1?format=3")
+    if [[ $? == 0 ]]
+    then
+        text=$(echo "$text" | sed -E "s/\s+/ /g")
+        tooltip=$(curl -s "https://wttr.in/$1?format=4")
+        if [[ $? == 0 ]]
+        then
+            tooltip=$(echo "$tooltip" | sed -E "s/\s+/ /g")
+            echo "{\"text\":\"$text\", \"tooltip\":\"$tooltip\"}"
+            exit
+        fi
+    fi
+    sleep 2
+done
+echo "{\"text\":\"error\", \"tooltip\":\"error\"}"
