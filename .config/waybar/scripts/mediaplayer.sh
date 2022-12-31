9357bd3c6485765fe8418e22c8fa7be879b73b69
Levent Can Yilmaz <leventcanyilmaz@proton.me>
2022-12-31 18:39:23 +0100

    updated media script

--- .config/waybar/scripts/mediaplayer.sh
+++ .config/waybar/scripts/mediaplayer.sh
@@ -2,16 +2,16 @@
 
 player_status=$(playerctl --player=spotify,firefox status)
 player_name=$(playerctl metadata --format "{{ playerName }}")
-song=$(playerctl metadata --format "{{ artist }}\n{{ title }}\n{{ album }}")
-
+spotify_title=$(playerctl -p spotify metadata --format "{{ emoji(status) }} {{ artist }} - {{ title }} - {{ duration(position) }} | {{ duration(mpris:length) }}")
+firefox_title=$(playerctl metadata --format "{{ emoji(status) }} {{ artist }} - {{ title }}")
 firefox_status=$(playerctl --player=firefox status)
 
 if [ "$player_status" = "Playing" ] || [ "$player_status" = "Paused" ]
 then
     if [ "$player_name" = "spotify" ] || [ "$firefox_status" = "Paused" ]
     then
-        echo "$(playerctl -p spotify metadata --format "{{ emoji(status) }} {{ artist }} - {{ title }} - {{ duration(position) }} / {{ duration(mpris:length) }}")"
+        echo "$spotify_title"
     else
-        echo "$(playerctl metadata --format "{{ emoji(status) }} {{ artist }} - {{ title }}")"
+        echo "$firefox_title"
     fi
 fi
