ebd10a0bc3ba08d658a36392c3eefd3f98a19c61
Levent Can Yilmaz <leventcanyilmaz@proton.me>
2023-01-16 00:22:19 +0100

    changed script output

--- .config/waybar/scripts/checkupdates.sh
+++ .config/waybar/scripts/checkupdates.sh
@@ -6,19 +6,19 @@ updates=$((updates_pacman + updates_yay))
 
 if [ "$updates" -gt 0 ]; then
     if [ "$updates_yay" -eq 1 ]; then
-        echo "<span foreground='#72A07C'><big></big> $updates</span>"
+        echo "<span foreground='#99de1e'><big></big> $updates</span>"
         notify-send -u normal -i software-update-available-symbolic "$updates_yay update available from AUR" "$(yay -Qu --aur)"
     fi 
     if [ "$updates_pacman" -eq 1 ]; then
-        echo "<span foreground='#72A07C'><big></big> $updates</span>"
+        echo "<span foreground='#99de1e'><big></big> $updates</span>"
         notify-send -u normal -i software-update-available-symbolic "$updates_pacman update available from pacman" "$(checkupdates)"
     fi
     if [ "$updates_yay" -gt 1 ]; then
-        echo "<span foreground='#72A07C'><big></big> $updates</span>"
+        echo "<span foreground='#99de1e'><big></big> $updates</span>"
         notify-send -u normal -i software-update-available-symbolic "$updates_yay updates available from AUR" "$(yay -Qu --aur)"
     fi 
     if [ "$updates_pacman" -gt 1 ]; then
-        echo "<span foreground='#72A07C'><big></big> $updates</span>"
+        echo "<span foreground='#99de1e'><big></big> $updates</span>"
         notify-send -u normal -i software-update-available-symbolic "$updates_pacman updates available from pacman" "$(checkupdates)"
     fi 
 else
--- .config/waybar/scripts/mediaplayer.sh
+++ .config/waybar/scripts/mediaplayer.sh
@@ -2,14 +2,12 @@
 
 player_status=$(playerctl --player=spotify,firefox status)
 player_name=$(playerctl metadata --format "{{ playerName }}")
-spotify_title=$(playerctl -p spotify metadata --format "{{ emoji(status) }} {{ artist }} - {{ title }} - {{ duration(position) }} | {{ duration(mpris:length) }}")
-firefox_title=$(playerctl metadata --format "{{ emoji(status) }} {{ artist }} - {{ title }}")
+spotify_title=$(playerctl -p spotify metadata --format "<big></big> {{ artist }} - {{ title }} - {{ duration(position) }} | {{ duration(mpris:length) }}")
+firefox_title=$(playerctl metadata --format "<big></big> {{ artist }} - {{ title }}")
 firefox_status=$(playerctl --player=firefox status)
 
-if [ "$player_status" = "Playing" ] || [ "$player_status" = "Paused" ]
-then
-    if [ "$player_name" = "spotify" ] || [ "$firefox_status" = "Paused" ]
-    then
+if [ "$player_status" = "Playing" ] || [ "$player_status" = "Paused" ]; then
+    if [ "$player_name" = "spotify" ] || [ "$firefox_status" = "Paused" ]; then
         echo "$spotify_title"
     else
         echo "$firefox_title"
