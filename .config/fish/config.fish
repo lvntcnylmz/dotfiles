ac4528045eca1254aa40ac305506dcef2fd62efc
leventcanyilmaz <leventcanyilmaz@proton.me>
2023-06-26 22:02:29 +0300

    add fish shell config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     neofetch
-    oh-my-posh init fish --config /home/can/Documents/github/oh-my-posh/ohmyposh.omg.json | source
+    starship init fish | source
 end
