06709bce583b153fff1948393a476a568e5e4bf2
Levent Can Yilmaz <leventcanyilmaz@proton.me>
2023-09-06 13:01:32 +0300

    add waybar config

--- .config/waybar/scripts/cava.sh
+++ .config/waybar/scripts/cava.sh
@@ -1,43 +1,12 @@
-#! /bin/bash
 
-trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-bar="▁▂▃▄▅▆▇█"
-dict="s/;//g;"
-
-# creating "dictionary" to replace char with bar
-i=0
-while [ $i -lt ${#bar} ]
-do
-    dict="${dict}s/$i/${bar:$i:1}/g;"
-    i=$((i=i+1))
-done
-
-# make sure to clean pipe
-pipe="/tmp/cava.fifo"
-if [ -p $pipe ]; then
-    unlink $pipe
+#!/bin/bash
+is_cava_ServerExist=`ps -ef|grep -m 1 cava|grep -v "grep"|wc -l`
+if [ "$is_cava_ServerExist" = "0" ]; then
+	echo "cava_server not found" > /dev/null 2>&1
+#	exit;
+elif [ "$is_cava_ServerExist" = "1" ]; then
+  killall cava
 fi
-mkfifo $pipe
 
-# write cava config
-config_file="/tmp/polybar_cava_config"
-echo "
-[general]
-bars = 10
-framerate = 60
-
-[output]
-method = raw
-raw_target = $pipe
-data_format = ascii
-ascii_max_range = 7
-" > $config_file
-
-# run cava in the background
-cava -p $config_file &
-
-# reading data from fifo
-while read -r cmd; do
-    echo $cmd | sed $dict
-done < $pipe
+cava -p ~/.config/cava/config | sed -u 's/;//g;s/0/▁/g;s/1/▂/g;s/2/▃/g;s/3/▄/g;s/4/▅/g;s/5/▆/g;s/6/▇/g;s/7/█/g;'
