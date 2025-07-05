88b96132a0632a78a100fcea31089a786d737bc3
Levent C. Yilmaz <leventcanyilmaz@proton.me>
2025-07-05 09:31:38 +0300

    update waybar config

--- .config/waybar/scripts/waybar.sh
+++ .config/waybar/scripts/waybar.sh
@@ -1,22 +1,35 @@
-#!/usr/bin/env bash
+#!/bin/bash
+# Wait until Sway is ready
+# until swaymsg -t get_outputs &>/dev/null; do sleep 0.5; done
+#
+# # Function to (re)start Waybar based on connected displays
+# restart_waybar() {
+#   # Kill existing Waybar
+#   pkill -x waybar
+#   while pgrep -x waybar >/dev/null; do sleep 0.5; done
+#
+#   # Count active outputs
+#   DISPLAY_NUMBER=$(swaymsg -t get_outputs | jq -r '.[] | select(.active) | .name' | wc -l)
+#
+#   # Launch appropriate config
+#   if [[ $DISPLAY_NUMBER -ge 2 ]]; then
+#     notify-send -u normal -i settings "External monitor detected." "Loading external config."
+#     waybar -c "$HOME/.config/waybar/config-main.jsonc"
+#   else
+#     notify-send -u normal -i settings "Only laptop screen detected." "Loading laptop config."
+#     waybar -c "$HOME/.config/waybar/config-second.jsonc"
+#   fi
+# }
+#
+# # Initial launch
+# restart_waybar
+#
+# # Listen for output changes and restart Waybar accordingly
+# swaymsg -t subscribe '["output"]' | while read -r _; do
+#   restart_waybar
+# done
 
-while true; do
-  # Update display number
-  display_number=$(swaymsg -t get_outputs | grep -i name | wc -l)
-
-  # Terminate already running instances of waybar
-  pkill -x waybar
-
-  # Wait until the processes have been shut down
-  while pgrep -x waybar >/dev/null; do sleep 1; done
-
-  # Launch waybar with the appropriate configuration based on display number
-  if [[ $display_number -ge 2 ]]; then
-    waybar -c ~/.config/waybar/config-main.jsonc &
-  else
-    waybar -c ~/.config/waybar/config-second.jsonc &
-  fi
-
-  # Sleep for 1800 seconds/30 minutes before checking again
-  sleep 1800
-done
+# Check for an existing process
+if pgrep -x "waybar" >/dev/null; then
+  killall waybar
+fi
