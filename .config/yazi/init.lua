8a6961296d6e603cf615c167dd6b3c3748918ff4
Levent C. Yilmaz <leventcanyilmaz@proton.me>
2026-01-30 22:48:47 +0300

    update yazi config

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,5 +1,25 @@
 require("full-border"):setup({
 	type = ui.Border.PLAIN,
 })
-require("starship"):setup()
 require("git"):setup()
+
+Header:children_add(function()
+	if ya.target_family() ~= "unix" then
+		return ""
+	end
+	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
+end, 500, Header.LEFT)
+
+Status:children_add(function()
+	local h = cx.active.current.hovered
+	if not h or ya.target_family() ~= "unix" then
+		return ""
+	end
+
+	return ui.Line({
+		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+		":",
+		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+		" ",
+	})
+end, 500, Status.RIGHT)
